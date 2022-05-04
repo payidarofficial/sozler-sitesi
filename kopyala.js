dfe839a8a656da39909cfc571fa8e085a41a4def
Py <79402324+payidarofficial@users.noreply.github.com>
2022-05-04 03:31:11 +0300

    Update kopyala.js

--- kopyala.js
+++ kopyala.js
@@ -1,5 +1,5 @@
 function kopyala(element) {
-	var $temp = $(&quot;<input>;&quot;);
+	var $temp = $("<input>");
 	$(&quot;body&quot;).append($temp);
 	$temp.val($(element).text()).select();
 	document.execCommand(&quot;copy&quot;);
